8d832f878562c4c4640b7cb341c76c5156480e97
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2018-10-16 20:39:10 +0200

    Properly runs with castle as container, plus filter parameter for tests

--- umbraco-tests-castle.ps1
+++ umbraco-tests-castle.ps1
@@ -1,1 +1,10 @@
-&.\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe .\Our.Umbraco.Containers.Castle.UmbracoTests\bin\debug\Umbraco.Tests.dll
+param(
+	$where = ""
+)
+
+if ($where -eq "") {
+	&.\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe .\Our.Umbraco.Containers.Castle.UmbracoTests\bin\debug\Umbraco.Tests.dll
+} else {
+	&.\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe .\Our.Umbraco.Containers.Castle.UmbracoTests\bin\debug\Umbraco.Tests.dll `
+		--where $where
+}
