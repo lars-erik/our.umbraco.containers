899f6b0fb133ecb5917018f693e067d8cea5aa67
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2018-10-22 21:54:23 +0200

    Works pr 22-10

--- Our.Umbraco.Containers.Castle/CastleContainer.cs
+++ Our.Umbraco.Containers.Castle/CastleContainer.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dependencies;
+using System.Web.Http.Dispatcher;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Castle.Core;
+using Castle.MicroKernel;
 using Castle.MicroKernel.Lifestyle;
 using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.Resolvers;
 using Castle.MicroKernel.Resolvers.SpecializedResolvers;
 using Castle.Windsor;
 using Castle.Windsor.Diagnostics.Helpers;
+using Castle.Windsor.Installer;
+using Umbraco.Core.Components;
 using Umbraco.Core.Composing;
+using Umbraco.Web.Mvc;
 using IDependencyResolver = System.Web.Mvc.IDependencyResolver;
 
 namespace Our.Umbraco.Containers.Castle
@@ -25,7 +36,7 @@ namespace Our.Umbraco.Containers.Castle
             return castleContainer;
         }
 
-        private readonly Dictionary<Lifetime, LifestyleType> lifetimes = new Dictionary<Lifetime, LifestyleType>
+        private Dictionary<Lifetime, LifestyleType> lifetimes = new Dictionary<Lifetime, LifestyleType>
         {
             { Lifetime.Request, LifestyleType.PerWebRequest },
             { Lifetime.Scope, LifestyleType.Scoped },
@@ -33,14 +44,13 @@ namespace Our.Umbraco.Containers.Castle
             { Lifetime.Transient, LifestyleType.Transient },
         };
 
-        private readonly WindsorContainer container;
+        private WindsorContainer container;
 
         public object ConcreteContainer => container;
 
         public CastleContainer()
         {
             container = new WindsorContainer();
-            container.Register(Component.For<IContainer>().Instance(this));
             container.Register(Component.For<ILazyComponentLoader>().ImplementedBy<LazyOfTComponentLoader>());
             container.Kernel.Resolver.AddSubResolver(new CollectionResolver(container.Kernel));
         }
@@ -80,8 +90,15 @@ namespace Our.Umbraco.Containers.Castle
         public IEnumerable<Registration> GetRegistered(Type serviceType)
         {
             return container.Kernel.GetAssignableHandlers(serviceType)
-                .Select(x => new Registration(serviceType, x.GetComponentName())
+                .Select(x => new Registration(serviceType, x.ComponentModel.Name)
             );
+        }
+
+        public IEnumerable<Registration> GetRegistered()
+        {
+            return container.Kernel.GetAssignableHandlers(typeof(object))
+                .Select(x => new Registration(x.ComponentModel.Services.First(), x.ComponentModel.Name));
+
         }
 
         // fixme - Refactor to use Dictionary. Castle does not support nameless parameters. !?!?
@@ -127,7 +144,7 @@ namespace Our.Umbraco.Containers.Castle
             container.Register(
                 Component
                     .For(typeof(TService))
-                    .Named(typeof(TService).Name + "-factory-" + Guid.NewGuid().ToString("N"))
+                    .Named(typeof(TService).Name + "-impl-" + Guid.NewGuid().ToString("N"))
                     .UsingFactory<IContainer, TService>(f => factory(this))
                     .LifeStyle.Is(lifetimes[lifetime])
                     .IsDefault()
@@ -170,8 +187,8 @@ namespace Our.Umbraco.Containers.Castle
         // fixme Figure out if Castle returns things in same order as registered
         public void RegisterOrdered(Type serviceType, Type[] implementingTypes, Lifetime lifetime = Lifetime.Transient)
         {
-            foreach(var type in implementingTypes)
-            { 
+            foreach (var type in implementingTypes)
+            {
                 container.Register(
                     Component
                         .For(serviceType)
@@ -194,7 +211,13 @@ namespace Our.Umbraco.Containers.Castle
         // fixme - Figure out requirements
         public IContainer ConfigureForWeb()
         {
-            DependencyResolver.SetResolver(new WindsorDependencyResolver(container));
+            //Register<IFilteredControllerFactory>(x => new WindsorControllerFactory(this), Lifetime.Singleton);
+
+            //GlobalConfiguration.Configuration.DependencyResolver = new WindsorWebApiDependencyResolver(container);
+
+            GlobalConfiguration.Configuration.Services.Replace(
+                typeof(IHttpControllerActivator),
+                new WindsorCompositionRoot(this.container));
 
             return this;
         }
@@ -203,6 +226,136 @@ namespace Our.Umbraco.Containers.Castle
         public IContainer EnablePerWebRequestScope()
         {
             return this;
+        }
+    }
+
+    public class CastleWindsorComponent : IUmbracoUserComponent
+    {
+        public void Compose(Composition composition)
+        {
+
+        }
+
+        public Type InitializerType => typeof(CastleInitializer);
+
+        public class CastleInitializer : IComponentInitializer
+        {
+            private readonly IContainer container;
+
+            public CastleInitializer(IContainer container)
+            {
+                this.container = container;
+            }
+
+            public void Initialize()
+            {
+                //container.GetInstance<FilteredControllerFactoryCollectionBuilder>()
+                //    .Insert<WindsorControllerFactory>();
+            }
+        }
+
+        public void Terminate()
+        {
+        }
+    }
+
+    public class WindsorCompositionRoot : IHttpControllerActivator
+    {
+        private readonly IWindsorContainer container;
+
+        public WindsorCompositionRoot(IWindsorContainer container)
+        {
+            this.container = container;
+        }
+
+        public IHttpController Create(
+            HttpRequestMessage request,
+            HttpControllerDescriptor controllerDescriptor,
+            Type controllerType)
+        {
+            var controller =
+                (IHttpController)this.container.Resolve(controllerType);
+
+            request.RegisterForDispose(
+                new Release(
+                    () => this.container.Release(controller)));
+
+            return controller;
+        }
+
+        private class Release : IDisposable
+        {
+            private readonly Action release;
+
+            public Release(Action release)
+            {
+                this.release = release;
+            }
+
+            public void Dispose()
+            {
+                this.release();
+            }
+        }
+    }
+
+    public class WindsorWebApiDependencyResolver : System.Web.Http.Dependencies.IDependencyResolver
+    {
+        private readonly IWindsorContainer container;
+
+        public WindsorWebApiDependencyResolver(IWindsorContainer container)
+        {
+            this.container = container;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return container.Resolve(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return container.ResolveAll(serviceType).Cast<object>();
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new WindsorDependencyScope(container);
+        }
+    }
+
+    internal sealed class WindsorDependencyScope : IDependencyScope
+    {
+        private readonly IWindsorContainer _container;
+        private readonly IDisposable _scope;
+
+        public WindsorDependencyScope(IWindsorContainer container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException("container");
+            }
+            _container = container;
+            _scope = container.BeginScope();
+        }
+
+        public object GetService(Type t)
+        {
+            return _container.Kernel.HasComponent(t) ? _container.Resolve(t) : null;
+        }
+
+        public IEnumerable<object> GetServices(Type t)
+        {
+            return _container.ResolveAll(t).Cast<object>().ToArray();
+        }
+
+        public void Dispose()
+        {
+            _scope.Dispose();
         }
     }
 
@@ -229,13 +382,18 @@ namespace Our.Umbraco.Containers.Castle
     /// <summary>
     /// Possibly not needed at all?
     /// </summary>
-    public class WindsorControllerFactory : DefaultControllerFactory
+    public class WindsorControllerFactory : DefaultControllerFactory, IFilteredControllerFactory
     {
         private readonly IContainer container;
 
         public WindsorControllerFactory(IContainer container)
         {
             this.container = container;
+        }
+
+        public bool CanHandle(RequestContext request)
+        {
+            return true;
         }
 
         public override void ReleaseController(IController controller)
@@ -253,3 +411,4 @@ namespace Our.Umbraco.Containers.Castle
         }
     }
 }
+
